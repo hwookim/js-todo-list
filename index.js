4c2380ee26012d750dbb8d9dba564cb220ac5e92
hwookim <gusdn9754@gmail.com>
2023-01-25 17:41:07 +0900

    ADD: 기능 구현

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+const todoInput = document.querySelector(".todoInput");
+const addTodoButton = document.querySelector(".addTodoButton");
+const todoList = document.querySelector(".todoList");
+
+function addTodo(value) {
+  const newTodoItem = document.createElement("div");
+  newTodoItem.classList.add("todoItem");
+  newTodoItem.innerText = value;
+
+  todoList.appendChild(newTodoItem);
+  todoInput.value = "";
+}
+
+function handleKeyupInput(event) {
+  if (event.key !== "Enter") return;
+
+  addTodo(event.target.value);
+}
+
+function handleClickButton() {
+  addTodo(todoInput.value);
+}
+
+todoInput.addEventListener("keyup", handleKeyupInput);
+addTodoButton.addEventListener("click", handleClickButton);
